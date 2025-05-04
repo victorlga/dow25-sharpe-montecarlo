a85184caddc0e35be3f4249946d70dda8134fea9
Victor Assis <46487019+victorlga@users.noreply.github.com>
2025-05-04 18:27:59 -0300

    fix: remove date column from download

--- data/download_data.py
+++ data/download_data.py
@@ -9,13 +9,9 @@ tickers = [
 start_date = "2024-08-01"
 end_date = "2024-12-31"
 
-# Baixa todos os dados
 raw_data = yf.download(tickers, start=start_date, end=end_date)
 
-# Filtra apenas a parte "Close"
 close_data = raw_data["Close"]
 
-# Salva
-close_data.to_csv("dow_jones_close_prices_aug_dec_2024.csv")
+close_data.to_csv("dow_jones_close_prices_aug_dec_2024.csv", index=False)
 
-print("Somente preços de fechamento salvos em 'dow_jones_close_prices_aug_dec_2024.csv'")
